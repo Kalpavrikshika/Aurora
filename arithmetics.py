513c996d16d87f49f135875050d74fe583121092
root <root@DESKTOP-8E1QIF4.localdomain>
2018-02-27 12:48:42 -0800

    Initial project files

---
+++ arithmetics.py
@@ -0,0 +1,21 @@
+
+class Arithmetics:
+    def __init__(self):
+        pass
+    def addition(self, a, b):
+        self.a = a
+        self.b = b
+        return (self.a + self.b)
+    def subtraction(self, a, b):
+        self.a = a
+        self.b = b
+        return (self.a - self.b)
+    def multiplication(self, a, b):
+        self.a = a
+        self.b = b
+        return (self.a * self.b)
+    def division(self, a, b):
+        self.a = a
+        self.b = b
+        return (self.a / self.b)
+ 
